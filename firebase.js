446c6b7fc75c637b4f8d7cdc51bcb0049bbc21b2
keremeersoy <azimetkeremersoy@gmail.com>
2022-10-12 03:28:26 +0300

    checkout

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyANRKTR1WQvV-k8j5fKimTzYsre1-vE1Yc",
+	authDomain: "clone-918a5.firebaseapp.com",
+	projectId: "clone-918a5",
+	storageBucket: "clone-918a5.appspot.com",
+	messagingSenderId: "502230243370",
+	appId: "1:502230243370:web:42125ef6be25b2da230b53",
+};
--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  images: {
-    domains: ['links.papareact.com', 'fakestoreapi.com'],
-  },
-}
+	images: {
+		domains: ["links.papareact.com", "fakestoreapi.com"],
+	},
+	env: {
+		NEXT_PUBLIC_GOOGLE_ID: "502230243370-qam2h542qa82jdc73guhsp36pe2arr0s.apps.googleusercontent.com",
+		NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: "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",
+	},
+};
+
