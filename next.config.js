525463a8225760c2df76793911541a22256a76ac
keremeersoy <azimetkeremersoy@gmail.com>
2022-10-12 19:38:08 +0300

    fix:fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = {
 	env: {
 		NEXT_PUBLIC_GOOGLE_ID: "502230243370-qam2h542qa82jdc73guhsp36pe2arr0s.apps.googleusercontent.com",
 		NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: "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",
+		stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+		stripe_secret_key: process.env.STRIPE_SECRET_KEY,
 	},
 };
 
